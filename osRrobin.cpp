24f7a1e7a449294c5cf060614f5dad8b23be5772
Sri Somanjali <srisomanjali18@gmail.com>
2023-04-09 01:31:11 +0530

    Updated

--- osRrobin.cpp
+++ osRrobin.cpp
@@ -1,54 +1,81 @@
-// Round Robin Scheduling Algorithm for 6 processes
 #include <iostream>
 #include <iomanip>
+#include <thread>
+#include <chrono>
+#include <queue>
+#include <mutex>
+
 using namespace std;
 
 int main()
 {
-    int n, i, j, k, time, remain, flag = 0, time_quantum;
+    int n, i, time, remain, flag = 0, time_quantum;
     int wait_time = 0, turnaround_time = 0, at[10], bt[10], rt[10];
     cout << "Enter Total Process:" << endl;
     cin >> n;
     remain = n;
     for (i = 0; i < n; i++)
     {
-        cout << "Enter Arrival Time  Process Number " << i + 1 << endl;
-         cout << "Burst Time for Process Process Number " << i+1 << endl;
-        cin >> at[i];
+        cout << "Enter Arrival Time  " << i + 1 << endl;
+        cout << "Burst Time for Process " << i+1 << endl;
+        cin >> at[i]; 
         cin >> bt[i];
         rt[i] = bt[i];
     }
-    cout << "Enter Time Quantum:" << endl;
+    cout << "Enter Quantum Time:" << endl;
     cin >> time_quantum;
-    cout << "Process ID\t\tArrival Time\t Burst Time\t Turnaround Time\t Waiting Time" << endl;
-    for (time = 0, i = 0; remain != 0;)
+
+    queue<int> q;
+    mutex q_mutex;
+
+    for (i = 0; i < n; i++)
     {
-        if (rt[i] <= time_quantum && rt[i] > 0)
-        {
-            time += rt[i];
-            rt[i] = 0;
-            flag = 1;
-        }
-        else if (rt[i] > 0)
-        {
-            rt[i] -= time_quantum;
-            time += time_quantum;
-        }
-        if (rt[i] == 0 && flag == 1)
-        {
-            remain--;
-            cout << "Process[" << i + 1 << "]\t\t" << at[i] << "\t\t" << bt[i] << "\t\t " << time - at[i] << "\t\t\t " << time - at[i] - bt[i] << endl;
-            wait_time += time - at[i] - bt[i];
-            turnaround_time += time - at[i];
-            flag = 0;
-        }
-        if (i == n - 1)
-            i = 0;
-        else if (at[i + 1] <= time)
-            i++;
-        else
-            i = 0;
+        q_mutex.lock();
+        q.push(i);
+        q_mutex.unlock();
     }
+
+    cout << "Process ID\t\tArrival Time\t Burst Time\t Turnaround Time\t Waiting Time" << endl;
+
+    while (remain != 0)
+    {
+        q_mutex.lock();
+        if (!q.empty())
+        {
+            i = q.front();
+            q.pop();
+            q_mutex.unlock();
+            if (rt[i] <= time_quantum && rt[i] > 0)
+            {
+                this_thread::sleep_for(chrono::milliseconds(rt[i])); // simulate process execution
+                time += rt[i];
+                rt[i] = 0;
+                flag = 1;
+            }
+            else if (rt[i] > 0)
+            {
+                this_thread::sleep_for(chrono::milliseconds(time_quantum)); // simulate process execution
+                rt[i] -= time_quantum;
+                time += time_quantum;
+            }
+            if (rt[i] == 0 && flag == 1)
+            {
+                remain--;
+                cout << "Process[" << i + 1 << "]\t\t" << at[i] << "\t\t" << bt[i] << "\t\t " << time - at[i] << "\t\t\t " << time - at[i] - bt[i] << endl;
+                wait_time += time - at[i] - bt[i];
+                turnaround_time += time - at[i];
+                flag = 0;
+            }
+            q_mutex.lock();
+            q.push(i);
+            q_mutex.unlock();
+        }
+        else
+        {
+            q_mutex.unlock();
+        }
+    }
+
     cout << "Average Waiting Time= " << wait_time * 1.0 / n << endl;
     cout << "Avg Turnaround Time = " << turnaround_time * 1.0 / n << endl;
     return 0;
