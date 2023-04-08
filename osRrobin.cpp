ce433430dc5fc5f491b963227f89747705fee401
Sri Somanjali <srisomanjali18@gmail.com>
2023-04-09 01:44:37 +0530

    updated

--- osRrobin.cpp
+++ osRrobin.cpp
@@ -1,82 +1,60 @@
-#include <iostream>
-#include <iomanip>
-#include <thread>
-#include <chrono>
-#include <queue>
-#include <mutex>
-
+#include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-    int n, i, time, remain, flag = 0, time_quantum;
-    int wait_time = 0, turnaround_time = 0, at[10], bt[10], rt[10];
-    cout << "Enter Total Process:" << endl;
-    cin >> n;
-    remain = n;
-    for (i = 0; i < n; i++)
+    int nop, i, time , remain, flag = 0, qt;
+    int wt = 0 , tt = 0 , at[10], bt[10], rt[10];
+    cout<<"Enter the total no of processes:"<<endl;
+    cin>>nop;
+    remain = nop;
+    for(i=0; i < nop; i++)
     {
-        cout << "Enter Arrival Time  " << i + 1 << endl;
-        cout << "Burst Time for Process " << i+1 << endl;
-        cin >> at[i]; 
+        cout << "Enter Arrival Time for Process " << i+1 << ":" <<endl;
+        cin >> at[i];
+        cout << "Enter Burst Time for Process " << i+1 << ":" <<endl;
         cin >> bt[i];
         rt[i] = bt[i];
     }
-    cout << "Enter Quantum Time:" << endl;
-    cin >> time_quantum;
-
-    queue<int> q;
-    mutex q_mutex;
-
-    for (i = 0; i < n; i++)
+    cout << "Enter the Quantum Time :"<<endl;
+    cin >> qt;
+    cout << "\nGantt Chart:" << endl;
+    for(time = 0 ; remain != 0; )
     {
-        q_mutex.lock();
-        q.push(i);
-        q_mutex.unlock();
-    }
-
-    cout << "Process ID\t\tArrival Time\t Burst Time\t Turnaround Time\t Waiting Time" << endl;
-
-    while (remain != 0)
-    {
-        q_mutex.lock();
-        if (!q.empty())
+        if(rt[i] <= qt && rt[i] > 0)
         {
-            i = q.front();
-            q.pop();
-            q_mutex.unlock();
-            if (rt[i] <= time_quantum && rt[i] > 0)
-            {
-                this_thread::sleep_for(chrono::milliseconds(rt[i])); // simulate process execution
-                time += rt[i];
-                rt[i] = 0;
-                flag = 1;
-            }
-            else if (rt[i] > 0)
-            {
-                this_thread::sleep_for(chrono::milliseconds(time_quantum)); // simulate process execution
-                rt[i] -= time_quantum;
-                time += time_quantum;
-            }
-            if (rt[i] == 0 && flag == 1)
-            {
-                remain--;
-                cout << "Process[" << i + 1 << "]\t\t" << at[i] << "\t\t" << bt[i] << "\t\t " << time - at[i] << "\t\t\t " << time - at[i] - bt[i] << endl;
-                wait_time += time - at[i] - bt[i];
-                turnaround_time += time - at[i];
-                flag = 0;
-            }
-            q_mutex.lock();
-            q.push(i);
-            q_mutex.unlock();
+            time += rt[i];
+            rt[i] = 0;
+            flag = 1;
         }
+        else if(rt[i] > 0)
+        {
+            rt[i] -= qt;
+            time += qt;
+        }
+        if(rt[i] == 0 && flag == 1)
+        {
+            remain--;
+            cout<<"| P[" << i + 1 << "] ";
+            wt += time - at[i] - bt[i];
+            tt += time - at[i];
+            flag = 0;
+        }
+        if(i == nop - 1)
+            i = 0;
+        else if(at[i + 1] <= time)
+            i++;
         else
-        {
-            q_mutex.unlock();
-        }
+            i = 0;
     }
-
-    cout << "Average Waiting Time= " << wait_time * 1.0 / n << endl;
-    cout << "Avg Turnaround Time = " << turnaround_time * 1.0 / n << endl;
+    cout << "|\n\n";
+    cout << "Process\tArrival Time\tBurst Time\tTurnaround Time\tWaiting Time" << endl;
+    for(i=0; i<nop; i++)
+    {
+        cout<<"P["<<i+1<<"]\t"<<setw(8)<<at[i]<<setw(16)<<bt[i]<<setw(21)<<time-at[i]<<setw(20)<<time-at[i]-bt[i]<<endl;
+    }
+    cout << "Average Waiting Time= " << wt * 1.0 / nop << endl;
+    cout << "Average Turnaround Time = " << tt * 1.0 / nop << endl;
     return 0;
 }
